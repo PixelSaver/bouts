9c954ab841a3e1debb99a09567d2bfb5b96a9aae
PixelSaver <stephen.k@proton.me>
2026-07-05 23:28:33 -0700

    rewrote as rigidbody2d, and added input buffer for jumping

--- scenes/game/player.gd
+++ scenes/game/player.gd
@@ -1,28 +1,37 @@
-extends CharacterBody2D
+extends RigidBody2D
 class_name Player
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var acceleration := 400
+@export var jump_strength := 400
+var is_on_floor := false
+var _jump_buffer := 0.3
+var _jump_buffer_max := 0.3
 var input_dir : float
 var input_jump : bool
 
 func _physics_process(delta: float) -> void:
-	_handle_movement()
+	_jump_buffer -= delta
+	_handle_input()
 	if not multiplayer.is_server(): return
 	_process_movement(input_dir, input_jump, delta)
 
-func _handle_movement():
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	is_on_floor = false
+	for i in range(state.get_contact_count()):
+		var normal = state.get_contact_local_normal(i)
+		#var point = state.get_contact_local_position(i)
+		if normal.dot(Vector2.UP) > 0.999:
+			is_on_floor = true
+
+func _handle_input():
 	if not is_multiplayer_authority(): return # only client controls client player
-	var dir := Input.get_axis("ui_left", "ui_right")
-	var jump = Input.is_action_just_pressed("space")
+	var dir := Input.get_axis("left", "right")
+	var jump = Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")
 	if multiplayer.is_server():
 		input_dir = dir
 		input_jump = jump
 	else:
 		submit_input.rpc(dir, jump)
-	# Add the gravity.
-	#if is_multiplayer_authority():
-		#_update_position.rpc(self.position, self.velocity)
 
 @rpc("any_peer", "unreliable")
 ## Sending input from client to server
@@ -35,20 +44,21 @@ func submit_input(dir:float, jump:bool) -> void:
 func sync_state(pos:Vector2, vel:Vector2) -> void:
 	if multiplayer.is_server(): return # don't overwrite server's local player
 	global_position = pos
-	velocity = vel
+	linear_velocity = vel
 
 func _process_movement(dir:float, jump:bool, delta:float) -> void:
 	var can_jump := true
-	if not is_on_floor():
+	if not is_on_floor:
 		can_jump = false
-		velocity += get_gravity() * delta
 
-	if jump and can_jump:
-		velocity.y = JUMP_VELOCITY
+	if can_jump and (jump or _jump_buffer > 0.):
+		self.apply_central_impulse(Vector2.UP * jump_strength)
+		self._jump_buffer = self._jump_buffer_max
 	var direction := dir
-	velocity.x = direction * SPEED if direction else move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-	sync_state.rpc(global_position, velocity)
+	#velocity.x = direction * acc if direction else move_toward(velocity.x, 0, acceleration)
+	if direction:
+		self.apply_central_force(Vector2.RIGHT * direction * acceleration)
+	if multiplayer.is_server():
+		sync_state.rpc(global_position, linear_velocity)
 
 	
