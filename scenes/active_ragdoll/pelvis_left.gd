3e6a3c92c454c7a52acaa9c7391bc988891c651d
PixelSaver <stephen.k@proton.me>
2026-07-13 23:37:18 -0700

    Still don't know what I'm doing

--- scenes/active_ragdoll/pelvis_left.gd
+++ scenes/active_ragdoll/pelvis_left.gd
@@ -19,9 +19,13 @@ func _physics_process(delta: float) -> void:
 	var inertia_a := _a.inertia if _a.inertia > 0.0 else 1.0
 	var inertia_b := _b.inertia if _b.inertia > 0.0 else 1.0
 	var effective_inertia := (inertia_a * inertia_b) / (inertia_a + inertia_b)
-
 	var damping : float = 2.0 * damping_ratio * sqrt(stiffness * effective_inertia)
-	var torque : float = error * stiffness - relative_velocity * damping
+	
+	
+	var predicted_error = error - relative_velocity * delta
+	var denom = 1.0 + damping * delta / effective_inertia + stiffness * delta * delta / effective_inertia
+
+	var torque : float = (predicted_error * stiffness - relative_velocity * damping) / denom
 	torque = clamp(torque, -max_torque, max_torque)
 	_a.apply_torque(torque)
 	_b.apply_torque(-torque)
